83d8a143b0a8162819678e2f4971002de290fbdc
rbuchynska <buchynska.ruslana@gmail.com>
2024-06-06 18:10:01 +0300

    Add function that return ticket by ID

--- src/main/java/org/example/service/TicketService.java
+++ src/main/java/org/example/service/TicketService.java
@@ -23,5 +23,14 @@ public class TicketService {
         Ticket ticket10 = new Ticket("4231", "Hall 1", "132", Instant.now().getEpochSecond(), true, "A", 154.34, new BigDecimal("43.9"));
 
         tickets = Arrays.asList(ticket1, ticket2, ticket3, ticket4, ticket5, ticket6, ticket7, ticket8, ticket9, ticket10);
+
+        System.out.println(getTicketByID("4231"));
+    }
+
+    public static Ticket getTicketByID(String ID) {
+        return tickets.stream()
+                .filter(ticket -> ID.equals(ticket.getID()))
+                .findAny()
+                .orElse(null);
     }
 }
